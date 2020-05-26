3abc3608618c40570d960a3ccc36eab59aeec203
Alexander Tokarev <alex.tokarew@gmail.com>
2020-05-26 15:38:23 +0300

    Printing the result of search

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,12 @@ fn main() {
     println!("Number of CPUs: {}", cpus);
 
     let initial_state: State = State::new([
-        0x00052000,
-        0x11521111,
-        0x32222100,
-        0x33333353,
+        0x00532000,
+        0x11311111,
+        0x02321202,
+        0x23333320,
         0x44444444,
-        0x55221555
+        0x55255555
     ]);
 
     println!("Initial state is: {:?}", initial_state);
@@ -26,34 +26,17 @@ fn main() {
     //     analyse_cores(num_cores, &ZERO_STATE);
     // }
 
-    let path = search(initial_state, ZERO_STATE);
+    let path = search(&initial_state, &ZERO_STATE);
 
-    for mv in path {
-        if *mv < 6 {
-            println!("{} CW", mv)
-        } else {
-            println!("{} CCW", *mv % 6)
-        }
+    let mut current_state = initial_state;
+
+    for turn in path {
+        println!("Turn {} {}", turn % 6, turn < 6);
+        current_state = current_state.turn(turn % 6, turn < 6);
+        println!("{:?}", current_state);
     }
 
-    // let forward = vec![7, 0, 1, 2, 7, 8];
-    // let back = vec![2, 7, 6, 8, 0, 0];
-
-    // let mut current_state = initial_state;
-
-    // for turn in forward {
-    //     println!("Turn {} {}", turn % 6, turn < 6);
-    //     current_state = current_state.turn(turn % 6, turn < 6);
-    //     println!("{:?}", current_state);
-    // }
-
-    // for turn in back.iter().rev() {
-    //     println!("Turn {} {}", *turn % 6, *turn >= 6);
-    //     current_state = current_state.turn(*turn % 6, *turn >= 6);
-    //     println!("{:?}", current_state);
-    // }
-
-    // assert_eq!(current_state, ZERO_STATE);
+    assert_eq!(current_state, ZERO_STATE);
 }
 
 fn analyse_cores(num_cores: u128, initial_state: &State) {
@@ -80,14 +63,14 @@ fn analyse_cores(num_cores: u128, initial_state: &State) {
     println!("For {} cores remainders count are {:?}", num_cores, rem_counter);
 }
 
-fn search(initial_state: State, target_state: State) -> &'static [u8] {
+fn search(initial_state: &State, target_state: &State) -> Vec<usize> {
     //value is a turn 0..5 - cw, 6..11 - ccw
-    let mut forward_paths: HashMap<u128, u8> = HashMap::new();
+    let mut forward_paths: HashMap<u128, usize> = HashMap::new();
     forward_paths.insert(initial_state.hash, 255);
     let mut forward_queue: VecDeque<u128> = VecDeque::new();
     forward_queue.push_back(initial_state.hash);
 
-    let mut back_paths: HashMap<u128, u8> = HashMap::new();
+    let mut back_paths: HashMap<u128, usize> = HashMap::new();
     back_paths.insert(target_state.hash, 255);
     let mut back_queue: VecDeque<u128> = VecDeque::new();
     back_queue.push_back(target_state.hash);
@@ -95,24 +78,44 @@ fn search(initial_state: State, target_state: State) -> &'static [u8] {
     while !forward_queue.is_empty() && !back_queue.is_empty() {
         let forward_state_hash = forward_queue.pop_front().unwrap();
         if forward_paths.contains_key(&forward_state_hash) && back_paths.contains_key(&forward_state_hash) {
-            println!("Forward {:?}", forward_paths.get(&forward_state_hash));
-            println!("Back {:?}", back_paths.get(&forward_state_hash));
-            return &[];
+            return create_search_result(forward_state_hash, forward_paths, back_paths);
         } else {
             process(forward_state_hash, &mut forward_paths, &mut forward_queue);
         }
 
         let back_state_hash = back_queue.pop_front().unwrap();
         if forward_paths.contains_key(&back_state_hash) && back_paths.contains_key(&back_state_hash) {
-            println!("Forward {:?}", forward_paths.get(&back_state_hash));
-            println!("Back {:?}", back_paths.get(&back_state_hash));
-            return &[];
+            return create_search_result(back_state_hash, forward_paths, back_paths);
         } else {
             process(back_state_hash, &mut back_paths, &mut back_queue);   
         }
     }
 
-    return &[];
+    return vec![];
+}
+
+fn create_search_result(state_hash: u128, forward_paths: HashMap<u128, usize>, back_paths: HashMap<u128, usize>) -> Vec<usize> {
+    let mut result = VecDeque::<usize>::new();
+    let mut state = State::from_hash(state_hash);
+
+    while *forward_paths.get(&state.hash).unwrap() != 255 {
+        let turn = forward_paths.get(&state.hash).unwrap();
+        println!("Forward {}", turn);
+        result.push_front(*turn);
+        state = state.turn(*turn % 6, *turn >= 6);
+    }
+
+    state = State::from_hash(state_hash);
+
+    while *back_paths.get(&state.hash).unwrap() != 255 {
+        let turn = back_paths.get(&state.hash).unwrap();
+        println!("Back {}", turn);
+        let reversed_turn = if *turn >= 6 { *turn - 6 } else { *turn + 6 };
+        result.push_back(reversed_turn);
+        state = state.turn(*turn % 6, *turn >= 6);
+    }
+    
+    result.into_iter().collect()
 }
 
 struct Moves {
@@ -143,13 +146,13 @@ fn select_thread(state_hash: u128) -> usize {
     (state_hash % 8) as usize
 }
 
-fn process(state_hash: u128, paths: &mut HashMap<u128, u8>, queue: &mut VecDeque<u128>) {
+fn process(state_hash: u128, paths: &mut HashMap<u128, usize>, queue: &mut VecDeque<u128>) {
     let state = State::from_hash(state_hash);
     for turn in 0..12 {
         let turned_state = state.turn(turn % 6, turn < 6);
         let turned_state_hash = turned_state.hash;
         if !paths.contains_key(&turned_state_hash) {
-            paths.insert(turned_state.hash, turn as u8);
+            paths.insert(turned_state.hash, turn);
             queue.push_back(turned_state.hash);
         }
     }
